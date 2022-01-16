1415befb6d0b6adc827d424cc8b9a9fc8d188c48
Minira <85894818+MiniraAkhundova@users.noreply.github.com>
2022-01-16 15:00:41 +0400

    Add files via upload

---
+++ Task1/Task1/Method.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Method
+    {
+        /// <summary>
+        /// Here is located our function minRoofLength(int[] cars, int k)
+        /// 1.here we take res value's initial value as max value of integer 
+        /// 2.then we have to sort our array because our numbers are unordered
+        /// 3.we look at the case if cars array length =k .
+        /// if it does we just have to cars[cars.Length - 1] - cars[0] + 1;
+        /// 4. then in loop we have to to compare max value with cars[i + k - 1] - cars[i]
+        /// and we get an answer
+        /// </summary>
+        /// <param name="cars"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int carParkingRoof(int[] cars, int k)
+        {
+            ////The full explanation you will get in README file !!!!!!
+            int res = int.MaxValue;
+            Array.Sort(cars);
+            if (cars.Length == k)
+            {
+                return cars[cars.Length - 1] - cars[0] + 1;
+            }
+
+            for (int i = 0; i < cars.Length - k; i++)
+            {
+                res = Math.Min(res, cars[i + k - 1] - cars[i]); 
+            }
+            return res + 1;
+          
+        }
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        /// <summary>
+        /// Here by creating the object of Method class 
+        /// where is replaced our Function we can get the minimum length of the roof.
+        /// We have to pass to this function 2 pharametres,namely:
+        /// array of our cars that are parked 
+        /// and
+        /// the number roofs that are possible to cover by roof
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //The full explanation you will get in README file !!!!!!
+            int[] arr = new int[] { 6, 2, 12, 7 };
+            Method newObject = new Method();
+            Console.WriteLine($"The minimum length of the roof is : { newObject.carParkingRoof(arr, 3)}");
+        }
+    }
+}
